1c5eafbf896d1ea9dd65ada0864f0a43139023fd
joezerSmaniotto <joezersmaniotto5@gmail.com>
2022-08-05 06:14:29 -0300

    criação projeto

---
+++ src/main/java/br/edu/ifsul/ivet/IvetApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.ivet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IvetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsul/ivet/IvetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.ivet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
