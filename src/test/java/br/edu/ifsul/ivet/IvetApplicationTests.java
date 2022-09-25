c8a9759fb3f9d1bc692f7f0867aa6354a0df3c67
joezerSmaniotto <joezersmaniotto5@gmail.com>
2022-09-24 22:01:08 -0300

    add imports in classIvetApllicationTests

--- src/test/java/br/edu/ifsul/ivet/IvetApplicationTests.java
+++ src/test/java/br/edu/ifsul/ivet/IvetApplicationTests.java
@@ -3,6 +3,19 @@ package br.edu.ifsul.ivet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.runner.RunWith;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class IvetApplicationTests {
 
