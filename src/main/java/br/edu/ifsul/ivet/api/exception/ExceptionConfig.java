c2e3d3ea410bef58af5948d6ba9b5b7866081bf4
joezerSmaniotto <joezersmaniotto5@gmail.com>
2022-09-26 13:28:27 -0300

    Application ResponseEntityExceptionHandler

--- src/main/java/br/edu/ifsul/ivet/api/exception/ExceptionConfig.java
+++ src/main/java/br/edu/ifsul/ivet/api/exception/ExceptionConfig.java
@@ -1,13 +1,24 @@
 package br.edu.ifsul.ivet.api.exception;
 
+import net.bytebuddy.pool.TypePool;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.io.Serializable;
 
 
 @RestControllerAdvice // Classe que trata exeções, ela intercepta eventos de rest, inclusive as exeções,  e trata de forma generica.
-public class ExceptionConfig {
+public class ExceptionConfig extends ResponseEntityExceptionHandler {
     @ExceptionHandler({
             EmptyResultDataAccessException.class
     })
@@ -21,6 +32,21 @@ public class ExceptionConfig {
     })
     public ResponseEntity errorBadRequest(Exception ex){
         return ResponseEntity.badRequest().build();
+    }
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>( new ExceptionError("Operação não permitida"), HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+}
+
+class ExceptionError implements Serializable {
+    private String error;
+    ExceptionError(String error){
+        this.error = error;
+    }
+    public String getError(){
+        return error;
     }
 
 }
