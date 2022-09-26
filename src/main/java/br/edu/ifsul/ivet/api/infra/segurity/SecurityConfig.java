d3dd37d03c8ef939c6913131efb2f13ada0a2230
joezerSmaniotto <joezersmaniotto5@gmail.com>
2022-09-26 19:39:33 -0300

    custon class userDatails

--- src/main/java/br/edu/ifsul/ivet/api/infra/segurity/SecurityConfig.java
+++ src/main/java/br/edu/ifsul/ivet/api/infra/segurity/SecurityConfig.java
@@ -1,17 +1,27 @@
 package br.edu.ifsul.ivet.api.infra.segurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
+import org.springframework.stereotype.Service;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)// Acesso de segurança por metetod
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("userDetailsService")
+    private UserDetailsService userDetailsService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -25,13 +35,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+
        //autenticação em memória
         // enable in memory based authentication with a user named "user" and "admin"
-        auth
-          .inMemoryAuthentication().passwordEncoder(encoder)
-                .withUser("user").password(encoder.encode("user")).roles("USER")
-                .and()
-                .withUser("admin").password(encoder.encode("admin")).roles("USER", "ADMIN");
+//        auth
+//          .inMemoryAuthentication().passwordEncoder(encoder)
+//                .withUser("user").password(encoder.encode("user")).roles("USER")
+//                .and()
+//                .withUser("admin").password(encoder.encode("admin")).roles("USER", "ADMIN");
 
     }
 }
