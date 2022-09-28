19e9be1ea5d69051c913ecd65eb18e7c404f8933
joezerSmaniotto <joezersmaniotto5@gmail.com>
2022-09-28 16:31:30 -0300

    update get Info user

--- src/main/java/br/edu/ifsul/ivet/api/user/UserController.java
+++ src/main/java/br/edu/ifsul/ivet/api/user/UserController.java
@@ -1,10 +1,12 @@
 package br.edu.ifsul.ivet.api.user;
 
+import br.edu.ifsul.ivet.api.infra.security.jwt.JwtUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +32,7 @@ public class UserController {
     public UserDTO userInfo(@AuthenticationPrincipal User user) { //a anotação retorna o user logado
 
         //User userLoged = (User) JwtUtil.getUserDetails(); //outra forma de retornar o user logado (nesse projeto)
-
-        return UserDTO.create(user);
+        UserDetails userDetails = JwtUtil.getUserDetails();
+        return UserDTO.create((User) userDetails);
     }
 }
